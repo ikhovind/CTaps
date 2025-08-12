3f1a6413487ecac49d50471d3905d70e1a3b7497
Ingebrigt Hovind <ing.hovind@hotmail.com>
2025-08-12 12:52:10 +0200

    start on structure

---
+++ source/api/connections/connection/connection.h
@@ -0,0 +1,8 @@
+//
+// Created by ikhovind on 12.08.25.
+//
+
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#endif //CONNECTION_H
---
+++ source/api/connections/preconnection/preconnection.h
@@ -0,0 +1,8 @@
+//
+// Created by ikhovind on 12.08.25.
+//
+
+#ifndef PRECONNECTION_H
+#define PRECONNECTION_H
+
+#endif //PRECONNECTION_H
