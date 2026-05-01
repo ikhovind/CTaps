9c068aed0ecd2da9c2e94e74166943025bff31ca
Ingebrigt Hovind <ing.hovind@hotmail.com>
2026-05-01 13:47:31 +0200

    Fix Doxygen

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -1,5 +1,4 @@
 find_package(Doxygen)
-
 if(DOXYGEN_FOUND)
     include(FetchContent)
     FetchContent_Declare(
@@ -8,20 +7,15 @@ if(DOXYGEN_FOUND)
         URL https://github.com/jothepro/doxygen-awesome-css/archive/refs/heads/main.zip
     )
     FetchContent_MakeAvailable(doxygen-awesome-css)
-
-    # Save the location the files were cloned into
-    # This allows us to get the path to doxygen-awesome.css
     FetchContent_GetProperties(doxygen-awesome-css SOURCE_DIR AWESOME_CSS_DIR)
 
-    # Generate the Doxyfile
     set(DOXYFILE_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in)
     set(DOXYFILE_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
     configure_file(${DOXYFILE_IN} ${DOXYFILE_OUT} @ONLY)
 
     add_custom_target(docs
-            COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-            COMMENT "Generating API documentation with Doxygen..."
+            COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYFILE_OUT}
+            COMMENT "Generating HTML documentation with Doxygen..."
             VERBATIM)
 else()
     message(STATUS "Doxygen not found, documentation target will not be available")
